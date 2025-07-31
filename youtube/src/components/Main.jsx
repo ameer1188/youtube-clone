d89419cd3adc33f939a4b13319bca0098f533999
ameer suhail <ameershuailvp6@gmail.com>
2025-07-31 16:07:39 +0530

    add main.jsx and main.css

---
+++ youtube/src/components/Main.jsx
@@ -0,0 +1,10 @@
+export default function Main(){
+    return(
+        <main className="main">
+            <div className="continer">
+                <div className="side-bar"></div>
+                <div className="content"></div>
+            </div>
+        </main>
+    )
+}
