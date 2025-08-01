9f855d08d4b620fb972e643a8771eda8aae2d490
ameer suhail <ameershuailvp6@gmail.com>
2025-08-01 12:18:37 +0530

    sidebar.jsx and main.css updated. in sidebar.jsx added three divs and buttons and main.css updated grid sytem 9, 1fr to 1fr repeat(3, 2fr)

--- youtube/src/components/Sidebar.jsx
+++ youtube/src/components/Sidebar.jsx
@@ -1,7 +1,15 @@
 export default function SideBar(){
     return(
         <div className="side-bar">
-
+            <div className="sidebar-top">
+                <button>Home</button>
+                <button>Shorts</button>
+                <button>Subscription</button>
+                <hr/>
+            </div>
+            <div className="sidebar-midle">
+                
+            </div>
         </div>
     )
 }
