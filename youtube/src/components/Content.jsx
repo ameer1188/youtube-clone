d25df27e75803e1f5a345f72e5f01525c1a72078
ameer suhail <ameershuailvp6@gmail.com>
2025-08-06 18:13:21 +0530

    updated code

--- youtube/src/components/Content.jsx
+++ youtube/src/components/Content.jsx
@@ -1,31 +1,47 @@
 import React from "react"
 
-const videoData= [
-    {
-        title: "40 Bruatal Truth i wish I knew in my 20s about coding",
-        channel:"Web Development",
-        views:"7.2k views",
-        timeAge:"13:35",
-        overlay:"you are not dumb",
-        thumbnail:"https://i,ytimg.com/vi/BAmkE2Gj1ku/hqdefault.jpg"
-    },
-    {
-        title: "Learn JavaScript in 1 Hour",
-        channel: "Code Master",
-        views: "1.5M views",
-        timeAgo: "2 weeks ago",
-        duration: "60:00",
-        overlay: "Start Here",
-        thumbnail: "https://i.ytimg.com/vi/W6NZfCO5SIk/hqdefault.jpg"
-    }
+const videoData = [
+  {
+    title: "40 Brutal Truths I Wish I Knew in My 20s About Coding",
+    channel: "Web Development",
+    views: "7.2k views",
+    timeAgo: "13:35",
+    overlay: "You are not dumb",
+    thumbnail: "https://i.ytimg.com/vi/BAmkE2Gj1ku/hqdefault.jpg"
+  },
+  {
+    title: "Learn JavaScript in 1 Hour",
+    channel: "Code Master",
+    views: "1.5M views",
+    timeAgo: "2 weeks ago",
+    duration: "60:00",
+    overlay: "Start Here",
+    thumbnail: "https://i.ytimg.com/vi/W6NZfCO5SIk/hqdefault.jpg"
+  },
+  {
+    title: "Learn JavaScript in 1 Hour",
+    channel: "Code Master",
+    views: "1.5M views",
+    timeAgo: "2 weeks ago",
+    duration: "60:00",
+    overlay: "Start Here",
+    thumbnail: "https://i.ytimg.com/vi/W6NZfCO5SIk/hqdefault.jpg"
+  }
 ]
 
-export default function Content(){
-    return(
-        <>
-        <div className="card">
-            <img src="{videoData.thumbnail}" alt="Thumbnail" />
+export default function Content() {
+  return (
+    <div className="content">
+      {videoData.map((video, index) => (
+        <div className="card" key={index}>
+          <img src={video.thumbnail} alt="Thumbnail" />
+          <h4>{video.title}</h4>
+          <p>{video.channel}</p>
+          <p>{video.views}</p>
+          <p>{video.timeAgo || video.duration}</p>
+          <span>{video.overlay}</span>
         </div>
-        </>
-    )
+      ))}
+    </div>
+  )
 }
