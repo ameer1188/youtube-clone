67c265dba1f19e17c73d3d8746f089b4f73a9861
ameer suhail <ameershuailvp6@gmail.com>
2025-08-15 20:22:42 +0530

    updated main content css

--- youtube/src/App.jsx
+++ youtube/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import './main.css'
 import './filterbar.css'
 import './Card.css'
+import './main-content.css'
 import Nav from "./components/Navbar"
 import Main from './components/Main'
 export default function App(){
--- youtube/src/components/MainContetn.jsx
+++ youtube/src/components/MainContetn.jsx
@@ -13,8 +13,8 @@ export default function MainContent(){
             />
     })
     return(
-        <>
+        <div className="main-content">
         {cardElement}
-        </>
+        </div>
     )
 }
